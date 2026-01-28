11710b21ca3a8e923b4179f35dcd6b59e408c4f7
H82M19\Student <Student>
2026-01-28 11:35:10 +0700

    Exercise week 3

---
+++ reactjs/React_Fundamentals_JSX/components/StudentInfo.jsx
@@ -0,0 +1,10 @@
+export default function StudentInfo(props) {
+    return (
+        <>
+            <div className="info-ctn" style={{backgroundColor: "red"}}>
+                Hello, my name is Kai
+            </div>
+        </>
+    )
+}
+
